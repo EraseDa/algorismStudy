3044df56fbbf4d8b185d93d77a025372b53cc9ab
EraseDa <fkakrla123@gmail.com>
2023-07-07 10:00:00 +0900

    평균 성적 조작하기

--- src/test/java/algorism/study/dataStructure/ImplementationEx01.java
+++ src/test/java/algorism/study/dataStructure/ImplementationEx01.java
@@ -1,7 +1,10 @@
 package algorism.study.dataStructure;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 public class ImplementationEx01 {
 
@@ -15,5 +18,18 @@ public class ImplementationEx01 {
         charArray.toString();
     }
 
+    @Test
+    @DisplayName("성적 조작하기")
+    void averageChange(){
+        int num = 3;
+        int [] arr = {40,80,60};
+        Arrays.sort(arr);
+        int max = arr[num-1];
+        double sum = 0;
 
+        for(int i=0; i<num; i++) {
+            sum += (arr[i]/(double)max)*100;
+        }
+        Assertions.assertThat(sum/num).isEqualTo(75);
+    }
 }
