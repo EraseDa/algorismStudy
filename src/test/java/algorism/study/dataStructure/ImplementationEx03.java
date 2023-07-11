7cd680609cdca9bf0aff6bb899a0b436dfaefeb7
EraseDa <fkakrla123@gmail.com>
2023-07-11 14:38:11 +0900

    멀리뛰기

--- src/test/java/algorism/study/dataStructure/ImplementationEx03.java
+++ src/test/java/algorism/study/dataStructure/ImplementationEx03.java
@@ -1,5 +1,6 @@
 package algorism.study.dataStructure;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -36,4 +37,32 @@ public class ImplementationEx03 {
             copyArr[i-1] = big.multiply(small).divide(big.gcd(small)).intValue();
         }
     }
+
+    /*DP문제
+    * 효진이는 멀리 뛰기를 연습하고 있습니다. 효진이는 한번에 1칸, 또는 2칸을 뛸 수 있습니다.
+    * 칸이 총 4개 있을 때, 효진이는
+        (1칸, 1칸, 1칸, 1칸)
+        (1칸, 2칸, 1칸)
+        (1칸, 1칸, 2칸)
+        (2칸, 1칸, 1칸)
+        (2칸, 2칸)의 5가지 방법으로 맨 끝 칸에 도달할 수 있습니다.
+        * 멀리뛰기에 사용될 칸의 수 n이 주어질 때,
+        * 효진이가 끝에 도달하는 방법이 몇 가지인지 알아내,
+        * 여기에 1234567를 나눈 나머지를 리턴하는 함수, solution을 완성하세요.
+        * 예를 들어 4가 입력된다면, 5를 return하면 됩니다.*/
+
+    @Test
+    @DisplayName("멀리뛰기")
+    void jump(){
+        int n = 4;
+        long[] arr = new long[n];
+        //n==1일 경우 고려해야함 n==1이면 arr[1]은 에러
+        arr[0] = 1;
+        arr[1] = 2;
+        for(int i = 2; i<arr.length; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        Assertions.assertThat(arr[n-1]).isEqualTo(5);
+    }
+
 }
