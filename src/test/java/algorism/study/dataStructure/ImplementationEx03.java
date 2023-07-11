e4ecadcf0db162ab3caf1c322aa5c9de6e6c6434
EraseDa <fkakrla123@gmail.com>
2023-07-11 12:13:31 +0900

    N개의 최소공배수

--- src/test/java/algorism/study/dataStructure/ImplementationEx03.java
+++ src/test/java/algorism/study/dataStructure/ImplementationEx03.java
@@ -1,39 +1,39 @@
 package algorism.study.dataStructure;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.math.BigInteger;
+import java.util.Arrays;
 
 public class ImplementationEx03 {
-
-    /*피보나치 수는 F(0) = 0, F(1) = 1일 때, 1 이상의 n에 대하여 F(n) = F(n-1) + F(n-2) 가 적용되는 수 입니다.
-
-예를들어
-
-F(2) = F(0) + F(1) = 0 + 1 = 1  0 1 1 2 3 5 8 13
-F(3) = F(1) + F(2) = 1 + 1 = 2
-F(4) = F(2) + F(3) = 1 + 2 = 3
-F(5) = F(3) + F(4) = 2 + 3 = 5
-와 같이 이어집니다.
-
-2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 리턴하는 함수, solution을 완성해 주세요.
-*/
-
+    /*문제 설명
+    두 수의 최소공배수(Least Common Multiple)란 입력된 두 수의 배수 
+    중 공통이 되는 가장 작은 숫자를 의미합니다. 
+    예를 들어 2와 7의 최소공배수는 14가 됩니다. 정의를 확장해서, 
+    n개의 수의 최소공배수는 n 개의 수들의 배수 중 공통이 되는 가장 작은 숫자가 됩니다. 
+    n개의 숫자를 담은 배열 arr이 입력되었을 때 이 수들의 최소공배수를 반환하는 함수, 
+    solution을 완성해 주세요.
+    
+    제한 사항
+    arr은 길이 1이상, 15이하인 배열입니다.
+    arr의 원소는 100 이하인 자연수입니다.*/
     @Test
-    @DisplayName("피보나치 수")
-    void fiboTest() {
-        int n = 5;
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(0);
-        arr.add(1);
-        for(int i=2; i<=n; i++) {
-            int pre = arr.get(i-2)%1234567;
-            int next = arr.get(i-1)%1234567;
-            int answer = (pre+next)%1234567;
-            arr.add(answer);
+    @DisplayName("N개의 최소공배수")
+    void multiple(){
+        int[] arr = {2,6,8,14};
+        int[] copyArr = new int[arr.length];
+        for(int i = 0; i<arr.length; i++) {
+            copyArr[i] = arr[i];
+        }//배열 복사. 기존 배열을 건드리지 않기 위해 복사해둠
+        Arrays.sort(copyArr);
+
+        for(int i = copyArr.length-1; i>0; i--) {
+            //복사한 배열에 copyArr[i]와 copyArr[i-1]의 최대 공약수를 구함
+            BigInteger big = BigInteger.valueOf(copyArr[i]);
+            BigInteger small = BigInteger.valueOf(copyArr[i - 1]);
+            //둘의 최소공배수를 구한 후, 복사한 배열의 [i-1]에 집어넣기
+            copyArr[i-1] = big.multiply(small).divide(big.gcd(small)).intValue();
         }
-        Assertions.assertThat(arr.get(n)).isEqualTo(5);
     }
 }
